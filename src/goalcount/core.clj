4d4104dc562e7662fed529832a416a59bad90285
Rick Hall <rickhall2000@hotmail.com>
2014-06-30 22:33:24 -0400

    gradient descent not working

--- src/goalcount/core.clj
+++ src/goalcount/core.clj
@@ -1,7 +1,8 @@
 (ns goalcount.core
   (:require [goalcount.statfile :refer [get-data get-values]]
             [incanter.charts :refer [scatter-plot add-lines]]
-            [incanter.core :refer [view]]))
+            [incanter.core :refer [view]]
+            [clojure.core.matrix :as m]))
 
 (defn get-matrices [x-keys y-key]
   (let [data (get-data)
@@ -12,11 +13,53 @@
 (def goals (get-matrices [:for] :pts))
 (def played (get-matrices [:played] :rank))
 (def points (get-matrices [:pts] :rank))
+(def wins (get-matrices [:win] :pts))
 
 (defn plot-it [[X Y]]
   (view
    (scatter-plot X Y)))
+(comment
+  (plot-it goals)
+  (plot-it played)
+  (plot-it points))
 
-(plot-it goals)
-(plot-it played)
-(plot-it points)
+
+
+(defn trim [v]
+  [(first v)
+   (m/matrix :clatrix [(rest v)])])
+
+(defn apply-hypothois [h X]
+  (let [[theta0 theta-n] (trim h)
+       hn (m/mmul X (m/transpose theta-n))]
+    (m/add hn theta0)))
+
+(defn get-square-error [guess real]
+  (let [diffs (m/sub guess real)]
+    (m/mul diffs diffs)))
+
+(def h (m/matrix :clatrix [[1 2 3] ]))
+
+(def x (m/matrix :clatrix [[1] [3] [5] [0]]))
+
+(def y (m/matrix :clatrix [[3] [7] [11] [1]]))
+
+(def ans (apply-hypothois h x))
+
+(defn regress [X Y a n]
+  (let [m (m/row-count Y)]
+    (loop [Theta (vec (repeat (inc (m/column-count X)) 0)) n n]
+      (if (zero? n)
+        Theta
+        (let [ans (apply-hypothois Theta X)
+              diffs (m/sub ans Y)
+              sum-dif (m/esum diffs)
+              theta-0 (first Theta)
+              theta-n (rest Theta)
+              adjust-0 (* sum-dif (/ a m))
+              dx (m/mmul (m/transpose diffs) X)
+              adjust-x (m/mul dx (/ a m))
+              ]
+          (recur (reduce conj [(- theta-0 adjust-0)]
+                         (m/to-vector (m/sub theta-n adjust-x)))
+                   (dec n)))))))
