35d7eb0f7340a0be3fcc0f33bd2fe74d66f2b0bc
Rick Hall <rickhall2000@hotmail.com>
2014-07-01 07:42:18 -0400

    linear regression working, need to refactor

--- src/goalcount/core.clj
+++ src/goalcount/core.clj
@@ -2,7 +2,9 @@
   (:require [goalcount.statfile :refer [get-data get-values]]
             [incanter.charts :refer [scatter-plot add-lines]]
             [incanter.core :refer [view]]
-            [clojure.core.matrix :as m]))
+            [incanter.stats :refer [linear-model]]
+            [clojure.core.matrix :as m]
+            [clatrix.core :as cl]))
 
 (defn get-matrices [x-keys y-key]
   (let [data (get-data)
@@ -10,10 +12,16 @@
         Y (get-values [y-key] data)]
     [X Y]))
 
+(defn add-ones [x]
+  (let [width (first (cl/size x))
+        new-row (vec (repeat width 1))
+        new-mat (cl/matrix new-row)]
+    (cl/hstack new-mat x)))
+
 (def goals (get-matrices [:for] :pts))
-(def played (get-matrices [:played] :rank))
-(def points (get-matrices [:pts] :rank))
-(def wins (get-matrices [:win] :pts))
+#_(def played (get-matrices [:played] :rank))
+#_(def points (get-matrices [:pts] :rank))
+#_(def wins (get-matrices [:win] :pts))
 
 (defn plot-it [[X Y]]
   (view
@@ -23,43 +31,35 @@
   (plot-it played)
   (plot-it points))
 
-
-
-(defn trim [v]
-  [(first v)
-   (m/matrix :clatrix [(rest v)])])
-
-(defn apply-hypothois [h X]
-  (let [[theta0 theta-n] (trim h)
-       hn (m/mmul X (m/transpose theta-n))]
-    (m/add hn theta0)))
+(defn apply-hypothois [Theta X]
+  (m/mmul X (m/transpose Theta)))
 
 (defn get-square-error [guess real]
   (let [diffs (m/sub guess real)]
     (m/mul diffs diffs)))
 
-(def h (m/matrix :clatrix [[1 2 3] ]))
+(defn mse [diffs]
+  (let [m (m/row-count diffs)]
+    (-> (m/mul diffs diffs)
+        (cl/sum)
+        (/ (* 2 m)))))
 
-(def x (m/matrix :clatrix [[1] [3] [5] [0]]))
-
-(def y (m/matrix :clatrix [[3] [7] [11] [1]]))
-
-(def ans (apply-hypothois h x))
-
-(defn regress [X Y a n]
-  (let [m (m/row-count Y)]
-    (loop [Theta (vec (repeat (inc (m/column-count X)) 0)) n n]
-      (if (zero? n)
+(defn regress [x Y a i]
+  (let [m (first (cl/size Y))
+        X (add-ones x)]
+    (loop [Theta  (cl/zeros 1 (second (cl/size X))) i i]
+      (if (zero? i)
         Theta
-        (let [ans (apply-hypothois Theta X)
-              diffs (m/sub ans Y)
-              sum-dif (m/esum diffs)
-              theta-0 (first Theta)
-              theta-n (rest Theta)
-              adjust-0 (* sum-dif (/ a m))
-              dx (m/mmul (m/transpose diffs) X)
-              adjust-x (m/mul dx (/ a m))
+        (let [ans (cl/* X (cl/t Theta))
+              diffs (cl/- ans Y)
+              dx (cl/* (cl/t diffs) X)
+              adjust-x (cl/* dx (/ a m))
               ]
-          (recur (reduce conj [(- theta-0 adjust-0)]
-                         (m/to-vector (m/sub theta-n adjust-x)))
-                   (dec n)))))))
+          (recur (cl/- Theta adjust-x)
+                   (dec i)))))))
+
+(def plot1 (scatter-plot (first goals) (second goals)))
+#_(def ab (regress (first goals) (second goals) 0.0001 10000000))
+(def plot2 (add-lines plot1 (first goals)
+                      (:fitted (linear-model (second goals) (first goals)))))
+#_(view plot2)
