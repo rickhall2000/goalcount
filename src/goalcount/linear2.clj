a7924117f0cb0e68da2b5d6c58ac310efc7ec547
Rick Hall <rickhall2000@hotmail.com>
2014-07-10 13:38:24 -0400

    partway through refactoring

---
+++ src/goalcount/linear2.clj
@@ -0,0 +1,51 @@
+(ns goalcount.linear2
+  (:require [goalcount.statfile :refer [get-data get-values]]
+            [goalcount.calcs :refer [linear-regression]]))
+
+(defn get-matrices [x-keys y-key]
+  (let [data (get-data)
+        X (get-values x-keys data)
+        Y (get-values [y-key] data)]
+    [X Y]))
+
+(defn reg-epl [[X Y]]
+  (linear-regression X Y 0.0001 1000000))
+
+(defn print-results [label theta]
+  (println (str "** " label " **"))
+  (println theta))
+
+(->> (get-matrices [:win] :pts)
+    reg-epl
+    (print-results "wins->points"))
+
+
+(->> (get-matrices [:win :draw] :pts)
+     reg-epl
+     (print-results "wins+draws->points"))
+
+
+(def for-points (get-matrices [:for] :pts))
+(def fa-theta (reg-epl for-points))
+(println "** fa-theta **")
+(println fa-theta)
+
+(def for-against-pts (get-matrices [:for :against] :pts) )
+(def fap-theta (reg-epl goals))
+(println "** fap-theta **")
+(println fap-theta)
+
+(defn fap-fn [{:keys [for against]}]
+  (+ (* (second fap-theta) for)
+     (* (last fap-theta) against)
+     (first fap-theta)))
+
+(def win-draw-loss-points (get-matrices [:win :draw :loss] :pts))
+(def wdlp-theta (reg-epl win-draw-loss-points))
+(println "** wdlp-theta **")
+(println wdlp-theta)
+
+(def for-against-gd-pts (get-matrices [:for :against :gd] :pts))
+(def fagp-theta (reg-epl for-against-gd-pts))
+(println "** fagp-theta **")
+(println fagp-theta)
